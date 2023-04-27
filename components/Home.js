748d6db9f64d43762a9baf6c53986e0559a47e66
Timothé <huarttim@gmail.com>
2023-04-27 15:02:52 +0200

    signup creation jsx

--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,12 @@
 import styles from "../styles/Home.module.css";
 import Login from "./Login";
+import SignUp from "./SignUp";
 
 function Home() {
   return (
     <div>
       <Login />
+      <SignUp />
     </div>
   );
 }
--- components/SignUp.js
+++ components/SignUp.js
@@ -1,9 +1,42 @@
 import styles from "../styles/Login.module.css";
-
+import { useEffect, useState } from "react";
 function SignUp() {
+  const [signUpFirstname, setSignUpFirstname] = useState("");
+  const [signUpName, setSignUpName] = useState("");
+  const [signUpPassword, setSignUpPassword] = useState("");
   return (
-    <div>
-      <main className={styles.main}></main>
+    <div className={styles.signUp}>
+      <div className={styles.signUpContainer}>
+        <img className={styles.logo} src="logo.png" alt="logo twitter" />
+        <h3 className={styles.titleSignUp}>Create your Hackatweet account </h3>
+        <div className={styles.registerSection}>
+          <input
+            className={styles.inputField}
+            type="text"
+            placeholder="firstname"
+            id="signUpFirstname"
+            onChange={(e) => setSignUpFirstname(e.target.value)}
+            value={signUpFirstname}
+          />
+          <input
+            className={styles.inputField}
+            type="text"
+            placeholder="name"
+            id="signUpName"
+            onChange={(e) => setSignUpName(e.target.value)}
+            value={signUpName}
+          />
+          <input
+            className={styles.inputField}
+            type="text"
+            placeholder="password"
+            id="signUpPassword"
+            onChange={(e) => setSignUpPassword(e.target.value)}
+            value={signUpPassword}
+          />
+        </div>
+        <button className={styles.btnSignUp2}>Sign Up</button>
+      </div>
     </div>
   );
 }
