40a436a4313a4c8ca86dfa0a03b42ca211fcfecd
Timothé <huarttim@gmail.com>
2023-04-28 15:52:32 +0200

    continu the tweetItem

--- components/TweetItem.js
+++ components/TweetItem.js
@@ -3,10 +3,16 @@ import styles from "../styles/TweetItem.module.css";
 function TweetItem() {
   return (
     <div className={styles.main}>
-      <img className={styles.oeuf} src="oeuf.png" alt="logo oeuf" />
-      <div className={styles.userInfo}>
+      <div className={styles.header}>
+        <img className={styles.oeuf} src="oeuf.png" alt="logo oeuf" />
+
         <span className={styles.name}>Antoine</span>
-        <span>@AntoineLeProf</span>
+        <span className={styles.userName}>@AntoineLeProf</span>
+        <span className={styles.point}>.</span>
+        <span className={styles.hours}>5 hours</span>
+      </div>
+      <div className={styles.footer}>
+        <p>Welcome to #hackatweet guys 😀</p>
       </div>
     </div>
   );
