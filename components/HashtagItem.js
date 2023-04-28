64d7ae2633d6b15e6574e65c113c7e766fdec838
Timothé <huarttim@gmail.com>
2023-04-28 16:57:42 +0200

    overflow

---
+++ components/HashtagItem.js
@@ -0,0 +1,11 @@
+import styles from "../styles/HashtagItem.module.css";
+
+function HashtagItem() {
+  return (
+    <div className={styles.main}>
+      <h2 className={styles.title}>Trends</h2>
+    </div>
+  );
+}
+
+export default HashtagItem;
